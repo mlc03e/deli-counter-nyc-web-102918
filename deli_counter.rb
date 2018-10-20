32f1ab16d79f67ec3384d8ac35d66d460ee63b5c
Meghan Campbell <mlc03e@gmail.com>
2018-10-20 15:41:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(h)
+  h = []
+  if h.length == 0 
+    puts "The line is currently empty."
+  else 
+    [1, 2, 3].each |h|
+    text = "The line is currently:#{h}" 
+    puts text
+end
+end
+
+def now_serving(line)
+  var t = 0
+  if line.length == 0 
+    return "There is nobody waiting to be served!"
+  else  
+    return "Currently serving " line.shift "."
+    t += 1
+   end 
+  end
+  
+
